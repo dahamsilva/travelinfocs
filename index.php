2fcdf429a780e6649309663e698c6a4d574a0f62
Daham_105018 <Daham_105018@105018-005L.dialog.dialoggsm.com>
2020-11-19 20:49:49 +0530

    Added Author

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	Theory by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Auther: Daham Silva
 -->
 <html>
 	<head>
